4ab9006afa55060a7dbbe759bd4bda9e822569a7
hijakd <jharyd@gmail.com>
2024-07-25 12:25:27 +1000

    drawGizmo corners, random positions for gameObjects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+// using RandomPos = RandomPosition;
 
-public class GameManager : MonoBehaviour
-{
+
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject goalObject;
+    public GameObject player;
+
+
+    [SerializeField] private int gridHeight;
+    [SerializeField] private int gridWidth;
+    private int halfHeight;
+    private int halfWidth;
+    private Vector3 spawnPosition;
+    private Vector3 goalPosition;
+    private Vector3 destination01;
+    private Vector3 rPostion;
+    private Vector2 xMinMax;
+    private Vector2 yMinMax;
+    
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(new Vector3(-gridWidth / 2, 1f, gridHeight / 2), new Vector3(0.75f, 2f, 0.75f));
+        Gizmos.DrawWireCube(new Vector3(gridWidth / 2, 1f, gridHeight / 2), new Vector3(0.75f, 2f, 0.75f));
+        Gizmos.DrawWireCube(new Vector3(gridWidth / 2, 1f, -gridHeight / 2), new Vector3(0.75f, 2f, 0.75f));
+        Gizmos.DrawWireCube(new Vector3(-gridWidth / 2, 1f, -gridHeight / 2), new Vector3(0.75f, 2f, 0.75f));
+    }
+
+    private void Awake() {
+        halfHeight = gridHeight / 2;
+        halfWidth = gridWidth / 2;
+        xMinMax.x = -halfWidth;
+        xMinMax.y = halfWidth;
+        yMinMax.x = -halfHeight;
+        yMinMax.y = halfHeight;
+
+        goalPosition = RandomPosition.Position(xMinMax, yMinMax);
+        spawnPosition = RandomPosition.Position(xMinMax, yMinMax);
+        destination01 = RandomPosition.Position(xMinMax, yMinMax);
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
+
+
+
 }
